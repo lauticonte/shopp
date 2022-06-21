8dc9646ff52aa1f60a219172d51c50cc69fc4b1f
Lautaro Conte <lauty.conte@gmail.com>
2022-06-21 19:52:44 -0300

    :white_check_mark: Agrega tests de integracion para aumentar el coverage

--- tests/integration/request-home.spec.js
+++ tests/integration/request-home.spec.js
@@ -27,3 +27,60 @@ test('Deberia utilizar bootstrap 5.2', async () => {
 
     expect(html).toMatch('bootstrap@5.2.0');
 });
+
+test('Deberia aparecer Placard cuando hago un fetch del producto 1', async () => {
+    const port = instance.address().port;
+    const params = new URLSearchParams();
+
+    params.append('productid',1);
+    const resp = await fetch(`http://localhost:${port}/cart?${params}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            productid: 1,
+        }),
+    });
+    const html = await resp.text();
+    expect(html).toMatch('Placard');
+});
+
+test('Deberia retornar 200 ok cuando elimino un producto del carrito', async () => {
+    const port = instance.address().port;
+    const params = new URLSearchParams();
+
+    params.append('productid',1);
+    await fetch(`http://localhost:${port}/cart?${params}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            productid: 1,
+        }),
+    });
+
+    params.append('productid',1);
+    const resp1 = await fetch(`http://localhost:${port}/cart/delete?${params}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            productid: 1,
+        }),
+    });
+
+    expect(resp1.status).toBe(200);    
+});
+
+test('Deberia mostrarme los productos con descuento', async () => {
+    const port = instance.address().port;
+    const resp = await fetch(`http://localhost:${port}/discount`);
+
+    const html = await resp.text();
+    expect(html).toMatch('Placard');
+    expect(html).toMatch('Mesa');
+    
+});
